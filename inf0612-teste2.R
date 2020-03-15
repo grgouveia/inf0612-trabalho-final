069b192465ffb3feae167a9ca87738b8d9f29d4e
Guilherme Gouveia <guilherme.gouveia@dextra-sw.com>
2020-03-15 17:07:57 -0300

    First commit with team names

---
+++ inf0612-teste2.R
@@ -0,0 +1,24 @@
+#------------------------------------------------#
+# INF-0611 Recuperacao de Informacao             #
+#                                                #
+# Trabalho Avaliativo 2                          #
+#------------------------------------------------#
+# Nome COMPLETO Aluna (o) 1: Guilherme Ramos     #
+#                             Gouveia            #
+#                                                #
+# Nome COMPLETO Aluna (o) 2: Marina Abichabki    #
+#                                     Pivato     #
+#                                                #
+# Nome COMPLETO Aluna (o) 3: Paola São Thiago    #
+#                                                #
+#                                                #
+# Nome COMPLETO Aluna (o) 4:                     #
+#                                                #
+#                                                #
+#------------------------------------------------#
+
+#--------------------------------------------------------------#
+#     Configuracao dos arquivos, libs e funções auxiliares     #
+#--------------------------------------------------------------#
+install.packages("ecodist")
+setwd("~/studies/mdc/INF-0612-I/teste2")
