2f1aad91883bc6605d6a0f42b5ad6204e59c3b67
marinaapivato <marinapivato@yahoo.com.br>
2020-03-18 21:42:16 -0300

    Atualizando dev

--- inf0612-teste2.R
+++ inf0612-teste2.R
@@ -102,5 +102,124 @@ sort(cepagri[cepagri$vento < 5,3])
 #ocorrem valores proximos de 0, entao 0 parece um valor valido
 #sobre o valor mais alto, 147, pesquisando na internet foi uma medicao verifica
 
+#-----------------------------------------------#
+#        Análise registros duplicados          #
+#-----------------------------------------------#
 
-## Analisar registros duplicados
+install.packages('tidyverse')
+library(tidyverse)
+
+#Retorna as linhas duplicadas do data frame
+cepagri[duplicated(cepagri),]
+
+# Exemplo de filtragem de uma linha duplidada
+cepagri[cepagri$horario == '2015-01-23 09:24:00',]
+
+#Remove linhas duplicadas
+cepagri <- cepagri[!duplicated(cepagri),]
+
+#verifica se ainda tem linhas duplicadas
+cepagri[duplicated(cepagri),]
+
+
+#-----------------------------------------------#
+#       Início Análise dos dados                #
+#-----------------------------------------------#
+
+#Temperatura média de cada mês
+temp_media <- tapply(cepagri$temp , cepagri$mes , mean)
+temp_media <- round(temp_media)
+temp_media
+
+
+#----------------Medidas de posição com a base tratada
+summary(cepagri)
+
+library(ggplot2)
+
+##########################################################
+##  O código do Boxplot pode ser melhorado e otimizado  ##
+##########################################################
+
+#boxplot
+cepagri2015 <- cepagri[cepagri$ano == 2015, ]
+cepagri2016 <- cepagri[cepagri$ano == 2016, ]
+cepagri2017 <- cepagri[cepagri$ano == 2017, ]
+cepagri2018 <- cepagri[cepagri$ano == 2018, ]
+cepagri2019 <- cepagri[cepagri$ano == 2019, ]
+
+
+cepagri2015$mes <- as.factor(cepagri2015$mes)
+cepagri2016$mes <- as.factor(cepagri2016$mes)
+cepagri2017$mes <- as.factor(cepagri2017$mes)
+cepagri2018$mes <- as.factor(cepagri2018$mes)
+cepagri2019$mes <- as.factor(cepagri2019$mes)
+
+ggplot(cepagri2015,
+       aes(x = mes , y = temp , group = mes)) +
+    geom_boxplot ()
+
+ggplot(cepagri2016,
+       aes(x = mes , y = temp , group = mes)) +
+    geom_boxplot ()
+
+ggplot(cepagri2017,
+       aes(x = mes , y = temp , group = mes)) +
+    geom_boxplot ()
+
+ggplot(cepagri2018,
+       aes(x = mes , y = temp , group = mes)) +
+    geom_boxplot ()
+
+ggplot(cepagri2019,
+       aes(x = mes , y = temp , group = mes)) +
+    geom_boxplot ()
+
+#----------------Medidas de Dispersão
+
+# Desvio padrão
+dp <- c()
+media <- c()
+coef_var <- c()
+for(i in 2:5){
+    #Calculo desvio padrão para colunas 2:5
+    dp <- round(c(dp,sd(cepagri[,i],na.rm = TRUE)),2)
+    #calculo média colunas 2:5
+    media <-round(c(media, mean(cepagri[,i],na.rm = TRUE)))
+    
+}
+#Coeficiente de variação
+coef_var <- round(c(coef_var, (dp/media)*100),2)
+# Tabela que mostra a média, desvio padrão e coeficiente de variação de cada coluna
+variaveis <- c('temp','vento','umid','sensa')
+medidas_dispersao <-data.frame(variaveis,media,dp,coef_var); medidas_dispersao
+
+
+#-------------------------Histogramas
+library(ggplot2)
+library(dplyr)
+
+# Histograma de cada coluna 
+hist(cepagri$temp, col = 'green', main = 'Histograma Temperatura', xlab = 'Temperatura', ylab = 'Frequência')
+hist(cepagri$sensa, col = 'red', main = 'Histograma Sensação Térmica', xlab = 'sensação térmica', ylab = 'Frequência')
+hist(cepagri$vento, col = 'gray', main = 'Histograma Vento', xlab = 'Vento', ylab = 'Frequência')
+hist(cepagri$umid, col = 'blue', main = 'Histograma Umidade', xlab = 'Umidade', ylab = 'Frequência')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
